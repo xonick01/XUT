3fe01463452d640dab16ba71e5f655f862138b1e
xonick01 <98373098+xonick01@users.noreply.github.com>
2025-04-05 00:49:41 -0600

    Update XUS.sh

--- XUS.sh
+++ XUS.sh
@@ -1,12 +1,15 @@
 echo "Xonick's Ubuntu Tweaks Script"
 #Software Add/Remove
-curl -fsSL https://ppa.floorp.app/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
-sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list "https://ppa.floorp.app/Floorp.list"
+
 sudo apt -y update
-sudo apt -y install gnome-tweaks gnome-shell-extension-manager gnome-shell-extension-user-theme floorp
+sudo apt -y install gnome-tweaks gnome-shell-extension-manager gnome-shell-extension-user-theme curl
 sudo apt -y remove gnome-characters gnome-font-viewer gnome-characters gnome-logs gnome-user-docs yelp software-properties-* gnome-accessibility-themes gnome-calculator gnome-clocks gnome-system-monitor gnome-text-editor gnome-keyring* evince libreoffice* firefox*
 sudo apt -y upgrade
 sudo apt -y autoremove
+curl -fsSL https://ppa.floorp.app/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
+sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list "https://ppa.floorp.app/Floorp.list"
+sudo apt -y update
+sudo apt -y install floorp
 
 #UI Tweaks
 mkdir ~/.themes
